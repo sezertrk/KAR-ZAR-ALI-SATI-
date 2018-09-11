60c06b4d5fdf2fa2de648b7d11aa0d2048e0de5d
sezertrk <43032011+sezertrk@users.noreply.github.com>
2018-09-11 14:34:20 +0300

    Add files via upload

---
+++ KAR ZAR ALIŞ SATIŞ/KAR ZAR ALIŞ SATIŞ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace KAR_ZAR_ALIŞ_SATIŞ
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
